fc3ae632285382c4d39311066896963d134d8c01
Dima939 <47751771+Dima939@users.noreply.github.com>
2022-03-28 12:25:28 +0300

    Update settings.py

--- delivery/settings.py
+++ delivery/settings.py
@@ -1,9 +1,12 @@
+import os
 import os.path
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+THIS_FOLDER = os.path.dirname(os.path.abspath(__file__))
+my_file = os.path.join(THIS_FOLDER, 'manage.py')
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
